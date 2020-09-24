374c44821b41062d36cbdf9f7ac76016e0e4002d
Wietse Wind <mail@wietse.com>
2020-09-24 14:53:33 +0200

    Smaller images, add ecosystem

--- src/views/Focus.vue
+++ src/views/Focus.vue
@@ -4,7 +4,7 @@
 
     <div class="px-2">
       <div class="row">
-        <div class="col-12 col-sm-8 col-lg-9 pb-5">
+        <div class="col-12 col-sm-9 col-lg-10 pb-5">
           <h4><b>Driving research and development and contributing to the code that is the core of the XRP Ledger.</b></h4>
           <ul>
             <li>Developer relations</li>
@@ -14,14 +14,14 @@
             <li>Fraud monitoring and prevention</li>
           </ul>
         </div>
-        <div class="col-12 col-md-4 col-lg-3 pb-5">
+        <div class="col-12 col-md-3 col-lg-2 pb-5">
           <img src="img/research.jpg" alt="" class="img-fluid rounded">
         </div>
 
-        <div class="col-12 col-md-4 col-lg-3 pb-5">
+        <div class="col-12 col-md-3 col-lg-2 pb-5">
           <img src="img/infra.jpg" alt="" class="img-fluid rounded">
         </div>
-        <div class="col-12 col-sm-8 col-lg-9 pb-5">
+        <div class="col-12 col-sm-9 col-lg-10 pb-5">
           <h4><b>Ensuring reliable transaction submissions and data querying to so everyone has reliable access to the XRP Ledger.</b></h4>
           <ul>
             <li>High-availability servers provide 24/7/365 access for anyone to query the full ledger history or submit transactions</li>
@@ -31,7 +31,7 @@
           </ul>
         </div>
 
-        <div class="col-12 col-sm-8 col-lg-9 pb-5">
+        <div class="col-12 col-sm-9 col-lg-10 pb-5">
           <h4><b>Social Impact &amp; Engagement.</b></h4>
           <ul>
             <li>Engaging businesses, non-profits, and regulators to build synergy within and across key sectors in order to broaden our social impact</li>
@@ -39,8 +39,20 @@
             <li>Collaboration with UBRI partners</li>
           </ul>
         </div>
-        <div class="col-12 col-md-4 col-lg-3 pb-5">
+        <div class="col-12 col-md-3 col-lg-2 pb-5">
           <img src="img/social.jpg" alt="" class="img-fluid rounded">
+        </div>
+
+        <div class="col-12 col-md-3 col-lg-2 pb-5">
+          <img src="img/events.jpg" alt="" class="img-fluid rounded">
+        </div>
+        <div class="col-12 col-sm-9 col-lg-10 pb-5">
+          <h4><b>Connecting diverse stakeholders to cultivate a thriving, vibrant XRP Ledger ecosystem.</b></h4>
+          <ul>
+            <li>Strategic, coordinated engagement to further XRP Ledger adoption</li>
+            <li>Collaboration with diverse stakeholders, such as businesses and universities</li>
+            <li>Fund raising</li>
+          </ul>
         </div>
       </div>
     </div>
